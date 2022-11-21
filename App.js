9b0ea862c3e19b4c98b6941ef2ffffecedb0f285
talhaansari77 <talhaprince09@gmail.com>
2022-11-21 22:51:41 +0500

    added splash&icons

--- App.js
+++ App.js
@@ -1,18 +1,23 @@
 import {View, Text, LogBox} from 'react-native';
-import React from 'react';
+import React, { useEffect } from 'react';
+import SplashScreen from 'react-native-splash-screen';
 import CustomText from './src/components/CustomText';
 import {Montserrat} from './src/utils/Fonts';
 import RootNavigator from './src/routes';
 import SendAmountScreen from './src/screens/Main/SendAmountScreen/SendAmountScreen';
 import WalletScreen from './src/screens/Main/WalletScreen/WalletScreen';
 import RecivedAmountScreen from './src/screens/Main/RecivedAmountScreen/RecivedAmountScreen';
-import { Provider } from 'react-redux';
-import { store } from './src/redux/store';
+import {Provider} from 'react-redux';
+import {store} from './src/redux/store';
 
 LogBox.ignoreLogs(['VirtualizedLists', 'Warning:...']);
 LogBox.ignoreAllLogs();
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <Provider store={store}>
       <RootNavigator />
