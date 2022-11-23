d233a685c36818d7316c8c20f4f49525502978b4
talhaansari77 <talhaprince09@gmail.com>
2022-11-24 03:39:59 +0500

    changes

--- src/screens/Main/AddDesign/AddDesignScreen.js
+++ src/screens/Main/AddDesign/AddDesignScreen.js
@@ -1,27 +1,31 @@
-import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import CustomText from '../../../components/CustomText'
-import { Spacer } from '../../../components/Spacer'
-import DesignForm from './Molecules/DesignForm'
-import { colors } from '../../../utils/Colors'
-import DesignFormBottom from './Molecules/DesignFormBottom'
+import {SafeAreaView, ScrollView, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import CustomText from '../../../components/CustomText';
+import {Spacer} from '../../../components/Spacer';
+import DesignForm from './Molecules/DesignForm';
+import {colors} from '../../../utils/Colors';
+import DesignFormBottom from './Molecules/DesignFormBottom';
 
 const AddDesignScreen = () => {
   return (
-    <ScrollView showsVerticalScrollIndicator={false}>
-    <View style={{backgroundColor:colors.white, flex:1}}>
+    <View style={{backgroundColor: colors.white, flex: 1}}>
       <SafeAreaView>
         <Spacer height={5} />
-        <CustomText label={"Design Your Ads"} fontSize={15} alignSelf={"center"}/>
-        <Spacer height={10} />
-        <DesignForm />
-        <DesignFormBottom />
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <CustomText
+            label={'Design Your Ads'}
+            fontSize={15}
+            alignSelf={'center'}
+          />
+          <Spacer height={10} />
+          <DesignForm />
+          <DesignFormBottom />
+        </ScrollView>
       </SafeAreaView>
-    </View> 
-    </ScrollView>
-  )
-}
+    </View>
+  );
+};
 
-export default AddDesignScreen
+export default AddDesignScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
