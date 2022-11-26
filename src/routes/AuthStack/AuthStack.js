72e614241a05d039ee9106d97680c42610bd89d2
talhaansari77 <talhaprince09@gmail.com>
2022-11-26 13:35:59 +0500

    fixes

--- src/routes/AuthStack/AuthStack.js
+++ src/routes/AuthStack/AuthStack.js
@@ -2,6 +2,7 @@ import {View, Text} from 'react-native';
 import React, {useEffect} from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import LoginScreen from '../../screens/Auth/Login/LoginScreen';
+import SignOutScreen from '../../screens/Main/SignOut/SignOutScreen';
 
 const AuthStack = ({navigation}) => {
   const Stack = createStackNavigator();
@@ -11,6 +12,7 @@ const AuthStack = ({navigation}) => {
       // initialRouteName={"SecurityWallet"}
     >
       <Stack.Screen name="LoginScreen" component={LoginScreen} />
+      <Stack.Screen name="SignOutScreen" component={SignOutScreen} />
   
 
 
